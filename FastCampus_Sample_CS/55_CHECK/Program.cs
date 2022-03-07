2bd8d30068ab5f0abfd6257cdcfe9b94fa9bdffb
OSHMOS <ojames9707@gmail.com>
2022-03-07 10:32:10 +0900

    55_Check

--- FastCampus_Sample_CS/55_CHECK/Program.cs
+++ FastCampus_Sample_CS/55_CHECK/Program.cs
@@ -4,15 +4,32 @@ namespace _CHECK
 {
     class MainClass
     {
+        // Random rnd = new Random();
         // rnd.Next(min, max)
-        // => (min <= rne < max)범위 중의 랜덤값
+        // => (min <= rnd < max)범위 중의 랜덤값
         public static void Main(string[] args)
         {
-            Random rnd = new Random();
-            int a = rnd.Next(0, 100);
-            int b = rnd.Next(0, 100);
+            //Console.WriteLine("a : {0} b : {1}", a, b);
 
-            Console.WriteLine("a : {0} b : {1}", a, b);
+            for (int i = 1; i < 6; i++)
+            {
+                Random rnd = new Random();
+                int a = rnd.Next(0, 100);
+                int b = rnd.Next(0, 100);
+
+                Console.WriteLine("{0} : 다음 두 수의 합은?(총 5문제)", i);
+                Console.WriteLine("{0} + {1} = ??", a, b);
+                int asumb = int.Parse(Console.ReadLine());
+
+                if (asumb == (a + b))
+                {
+                    Console.WriteLine("== 정답 ==");
+                }
+                else
+                {
+                    Console.WriteLine("오답(정답은 : {0})", (a + b));
+                }
+            }
         }
     }
 }
