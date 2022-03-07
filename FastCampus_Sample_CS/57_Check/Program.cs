e800be1fa56a3f422b4ff8029492e54272d86be4
OSHMOS <ojames9707@gmail.com>
2022-03-07 11:16:37 +0900

    57_Check

--- FastCampus_Sample_CS/57_Check/Program.cs
+++ FastCampus_Sample_CS/57_Check/Program.cs
@@ -6,7 +6,25 @@ namespace _Check
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int minGrade = 0;
+            int maxGrade = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("학생의 성적을 입력하세요 : ");
+                int grade = int.Parse(Console.ReadLine());
+                minGrade = grade;
+
+                if (grade > maxGrade)
+                {
+                    maxGrade = grade;
+                }
+                else if (grade < minGrade)
+                {
+                    minGrade = grade;
+                }
+            }
+
+            Console.WriteLine("최대값 : {0} 최소값 : {1}", maxGrade, minGrade);
         }
     }
 }
